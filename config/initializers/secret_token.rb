8df8ebc025e9854f471ec5d801b6e56c7862b3d2
Lixiang Ding <zhimadlx@gmail.com>
2014-08-01 17:01:41 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkCms::Application.config.secret_token = 'c9d0bf1ee738db58d1cf9e043368ff3001322207ca323dd995aed975c652ab640bfe5c832fa88e1dfd0b26becc9bf263818b82c6a1e15546abb1e7c0f0890b18'
